a939f544f97b2ed2814e442d9e4669178f794f8b
Paymon Haddad <paymonhaddad@Paymons-MacBook-Pro.local>
2023-06-26 21:21:39 -0700

    Added custom database solution for some reason lol

--- meme_generator/generate_meme/views.py
+++ meme_generator/generate_meme/views.py
@@ -2,6 +2,11 @@ from django.http import HttpResponse
 import os
 import openai
 import environ
+import requests
+import heapq
+from utils import MemeFileManager
+
+meme_file_manager = MemeFileManager()
 
 def index(request):
     '''
@@ -11,9 +16,6 @@ def index(request):
     generate html to return
     respond with html
     '''
-
-    # env = environ.Env()
-    # environ.Env.read_env()
 
     openai.api_key = os.getenv('OPENAI_API_KEY')
 
@@ -25,11 +27,23 @@ def index(request):
         size="256x256",
     )
 
+    url = response["data"][0]["url"]
 
-    print('yo we here bros')
-    print(response["data"][0]["url"])
+    meme_file_manager.download_meme(url)
 
     return HttpResponse("Hello, world. You're at the polls index.")
 
 def cheese_index(request):
     return HttpResponse("I like cheese.")
+
+
+# const std::string base_prompt =
+# R"(you are an edgy AI powered image macro generator.
+# you are given a meme prompt,
+# and in response you need to generate a top_text and bottom_text as a JSON object.
+# captions should be about 5 words each.
+# you should use the prompt to generate puns, employ ironic humor, and generally be witty in your response
+# Be concise. Return only the JSON object.
+# Prompt: )";
+
+# !openai_interface_->gen_image_and_download(req_body +  ", lineart, monochrome", image_path)
